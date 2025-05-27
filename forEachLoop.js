954f09a9d8dc37efaabedbc622aac7199fb16b05
Tanisha Dhakad <tanishadhakad17@gmail.com>
2025-05-27 23:38:06 +0530

    updated code of for-each with the concept of filter method

--- forEachLoop.js
+++ forEachLoop.js
@@ -47,3 +47,53 @@ myCoding.forEach( (item) => {
     console.log(item.languageName);
 } )
 
+
+
+//below code for coding.foreach will not work as for each loop does not return anything , it can just print .
+
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// } ) 
+// console.log(values);
+
+
+
+//lets consider more examples to understand filter , map and redced method of array 
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter( (num) => {
+    return num > 4
+} )
+
+//try to do same with for each
+const newNums1 = []
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums1.push(num)
+    }
+} )
+
+console.log(newNums1);
+
+
+//one more example of filter
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+  userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+  console.log(userBooks);
